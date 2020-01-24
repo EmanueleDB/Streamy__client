c6deca5df4c64787a1d13edce52077ddd4ff332b
LeleOi <leleoioioi@gmail.com>
2020-01-24 14:44:03 +0100

    StreamCreate send new streams to the server

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,9 @@
-//action creator
-import { SIGN_IN, SIGN_OUT } from "./types";
-export const signIn = (userId) => {
+import streams from "../apis/streams";
+import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from "./types";
+
+//action creators
+
+export const signIn = userId => {
   return {
     type: SIGN_IN,
     payload: userId
@@ -11,4 +14,9 @@ export const signOut = () => {
   return {
     type: SIGN_OUT
   };
+};
+
+export const createStream = formValues => async dispatch => {
+  const response = await streams.post("/streams", formValues);
+  dispatch({ type: CREATE_STREAM, payload: response.data });
 };
--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
+import reduxThunk from "redux-thunk";
 import reducers from "./reducers";
 import App from "./components/App";
 
 const composeEnhncer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhncer(applyMiddleware()));
+const store = createStore(
+  reducers,
+  composeEnhncer(applyMiddleware(reduxThunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>

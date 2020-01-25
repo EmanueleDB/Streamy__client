cfba582dabfc139ebbffb189aca8ccd1c9e654a7
LeleOi <leleoioioi@gmail.com>
2020-01-25 20:27:49 +0100

    StreamForm created to handle all the logic of StreamCreate and StreamEdit components

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,6 @@
 import streams from "../apis/streams";
+import history from "../history";
+
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -28,7 +30,7 @@ export const createStream = formValues => async (dispatch, getState) => {
   const { userId } = getState().auth;
   const response = await streams.post("/streams", { ...formValues, userId });
   dispatch({ type: CREATE_STREAM, payload: response.data });
-  
+  history.push("/");
 };
 
 export const fetchStreams = () => async dispatch => {
@@ -37,16 +39,16 @@ export const fetchStreams = () => async dispatch => {
 };
 
 export const fetchStream = id => async dispatch => {
-  const response = await streams.get(`/streams${id}`);
+  const response = await streams.get(`/streams/${id}`);
   dispatch({ type: FETCH_STREAM, payload: response.data });
 };
 
 export const editStream = (id, formValues) => async dispatch => {
-  const response = await streams.put(`/streams${id}`, formValues);
+  const response = await streams.put(`/streams/${id}`, formValues);
   dispatch({ type: EDIT_STREAM, payload: response.data });
 };
 
 export const deleteStream = id => async dispatch => {
-  await streams.delete(`/streams${id}`);
+  await streams.delete(`/streams/${id}`);
   dispatch({ type: DELETE_STREAM, payload: id });
 };
--- src/components/App.js
+++ src/components/App.js
@@ -6,19 +6,19 @@ import StreamDelete from "./streams/StreamDelete";
 import StreamList from "./streams/StreamList";
 import StreamShow from "./streams/StreamShow";
 import Header from "./Header";
-import createBrowserHistory from "../history";
+import history from "../history";
 
 const App = () => {
   return (
     <div className="ui container">
-      <Router history={createBrowserHistory}>
+      <Router history={history}>
         <div>
           <Header />
           <Route path="/" exact component={StreamList} />
-          <Route path="/streams/new" component={StreamCreate} />
-          <Route path="/streams/edit" component={StreamEdit} />
-          <Route path="/streams/delete" component={StreamDelete} />
-          <Route path="/streams/show" component={StreamShow} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit/:id" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </Router>
     </div>
